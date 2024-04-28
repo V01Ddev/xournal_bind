ca46791345cb21be3c3e0bba42213f16b52c05ea
V01Ddev <x.v.0.1.d.xhacking@gmail.com>
2024-04-28 14:49:44 +0300

    Very not work lol + gitignore

--- xb.c
+++ xb.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <linux/input.h>
 #include <linux/uinput.h>
+
+void find_active_events(int *working_events){
+
+    char dp[] = "/dev/input/event";
+
+    int Count = 0;
+
+    while (Count < 100){
+
+        char path[19];
+
+        strcpy(path, dp);
+
+        char strCount[2];
+
+        sprintf(strCount, "%d", Count);
+        strcat(path, strCount);
+
+        if (access(path, F_OK) == 0){
+            printf("%s exsists!\n", path);
+            int fd = open(path, O_RDONLY);
+
+            if (fd < 0){
+            }
+            else{
+                working_events[Count] = Count;
+                close(fd);
+            }
+            Count++;
+        }
+
+        else{
+            printf("Couldn't access %s\n", path);
+            Count++;
+        }
+    }
+}
+
+
 
 void emit(int fo, int type, int key, int value){
     struct input_event event;
@@ -17,26 +57,61 @@ void emit(int fo, int type, int key, int value){
 
 int main(){
 
-    struct js_event {
-        __u32 time;     /* event timestamp in milliseconds */
-        __s16 value;    /* value */
-        __u8 type;      /* event type */
-        __u8 number;    /* axis/button number */
+    struct input_event {
+        struct timeval time;
+        unsigned short type;
+        unsigned short code;
+        unsigned int value;
     };
-    
-    int fd = open("/dev/input/event3", O_RDONLY);
-    struct js_event e;
+
+    // Taking care of events and opening them...
+    printf("Finding events\n");
+
+    int active_events[99]= {0};
+    find_active_events(active_events);
+
+    int fd;
+
+    int events_array_size = (int) sizeof(active_events)/sizeof(int);
+    for (int i=0; i<events_array_size; i++){
+        printf("%d, ", active_events[i]);
+    }
+
+
+    // Opening all events that have been found
+    char dp[] = "/dev/input/event";
+    // Code ignores event0, very unlikely it is the keyboard...
+
+    for (int i=0; i <= events_array_size; i++){
+
+        if (active_events[i] != 0){
+            char path[19];
+
+            strcpy(path, dp);
+
+            char strNum[2];
+            sprintf(strNum, "%d", active_events[i]);
+            strcat(path, strNum);
+
+            printf("opening %s\n", path);
+
+            open(path, O_RDONLY);
+        }
+    }
+
+
+    // uinput setup
+    struct input_event e;
 
     int of = open ("/dev/uinput", O_WRONLY | O_NONBLOCK);
     struct uinput_setup usetup;
 
     if (of < 0){
-        printf("Unable to open /dev/uinput/\n");
+        printf("Unable to open /dev/uinput\n");
         return -1;
     } 
     else if (fd < 0){
-
-        printf("Unable to open /dev/\n");
+        printf("Unable to open /dev/event\n");
         return -1;
     }
 
@@ -53,16 +128,18 @@ int main(){
     ioctl(of, UI_DEV_SETUP, &usetup);
     ioctl(of, UI_DEV_CREATE);
 
-    for (int i=0; i<=3; i++){
+    // reading all inputs passed to fd
+    while (1) {
         read(fd, &e, sizeof(e));
 
         printf("----------\n");
-        printf("value --> %d\ntype --> %d\nnumber --> %d\n", e.value, e.type, e.number);
+        printf("value --> %d\ntype --> %d\ncode --> %d\n", e.value, e.type, e.code);
         printf("----------\n");
-
         sleep(1);
     }
 
     read(fd, &e, sizeof(e));
+
+    close(fd);
     return 0;
 }
